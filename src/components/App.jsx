a035b733db0d1be467bc0d90dcdd48c989e23554
Yen Maryna <113536885+6615marik@users.noreply.github.com>
2023-01-19 18:39:35 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,26 +4,26 @@ import user from '../data/user.json';
 import { Statatiscs } from './Statistics/Statistics';
 import data from '../data/data.json';
 
-import {FriendList} from './FriendList/FriendList'
-import friends from '../data/friends.json'
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json';
 
-import {TransactionHistory} from './TransactionHistory/TransactionHistory'
-import transactions from '../data/transactions.json'
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   const { username, tag, location, avatar, stats } = user;
   return (
-  <>
-    <Profile
-      username={username}
-      tag={tag}
-      location={location}
-      avatar={avatar}
-      stats={stats}/>
-    <Statatiscs title="Upload stats"stats={data}/>
-    <FriendList friends={friends} />
-    <TransactionHistory items={transactions} />
-  </>
-  )
+    <>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statatiscs title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
 };
-
